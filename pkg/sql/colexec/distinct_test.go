143defa38020c6c86972386282f35e06f8dfa768
Azhng <archer.xn@gmail.com>
2020-03-30 14:54:06 -0400

    backport benchmark

--- pkg/sql/colexec/distinct_test.go
+++ pkg/sql/colexec/distinct_test.go
@@ -13,149 +13,85 @@ package colexec
 import (
 	"context"
 	"fmt"
+	"math"
 	"testing"
 
 	"github.com/cockroachdb/cockroach/pkg/col/coldata"
 	"github.com/cockroachdb/cockroach/pkg/col/coltypes"
-	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
 	"github.com/cockroachdb/cockroach/pkg/util/randutil"
 )
 
-func TestSortedDistinct(t *testing.T) {
-	defer leaktest.AfterTest(t)()
-	tcs := []struct {
-		distinctCols []uint32
-		colTypes     []coltypes.T
-		numCols      int
-		tuples       []tuple
-		expected     []tuple
-	}{
-		{
-			distinctCols: []uint32{0, 1, 2},
-			colTypes:     []coltypes.T{coltypes.Float64, coltypes.Int64, coltypes.Bytes},
-			numCols:      4,
-			tuples: tuples{
-				{nil, nil, nil, nil},
-				{nil, nil, nil, nil},
-				{nil, nil, "30", nil},
-				{1.0, 2, "30", 4},
-				{1.0, 2, "30", 5},
-				{2.0, 2, "30", 4},
-				{2.0, 3, "30", 4},
-				{2.0, 3, "40", 4},
-				{2.0, 3, "40", 4},
-			},
-			expected: tuples{
-				{nil, nil, nil, nil},
-				{nil, nil, "30", nil},
-				{1.0, 2, "30", 4},
-				{2.0, 2, "30", 4},
-				{2.0, 3, "30", 4},
-				{2.0, 3, "40", 4},
-			},
-		},
-		{
-			distinctCols: []uint32{1, 0, 2},
-			colTypes:     []coltypes.T{coltypes.Float64, coltypes.Int64, coltypes.Bytes},
-			numCols:      4,
-			tuples: tuples{
-				{nil, nil, nil, nil},
-				{nil, nil, nil, nil},
-				{nil, nil, "30", nil},
-				{1.0, 2, "30", 4},
-				{1.0, 2, "30", 5},
-				{2.0, 2, "30", 4},
-				{2.0, 3, "30", 4},
-				{2.0, 3, "40", 4},
-				{2.0, 3, "40", 4},
-			},
-			expected: tuples{
-				{nil, nil, nil, nil},
-				{nil, nil, "30", nil},
-				{1.0, 2, "30", 4},
-				{2.0, 2, "30", 4},
-				{2.0, 3, "30", 4},
-				{2.0, 3, "40", 4},
-			},
-		},
-		{
-			distinctCols: []uint32{0, 1, 2},
-			colTypes:     []coltypes.T{coltypes.Float64, coltypes.Int64, coltypes.Bytes},
-			numCols:      4,
-			tuples: tuples{
-				{1.0, 2, "30", 4},
-				{1.0, 2, "30", 5},
-				{nil, nil, nil, nil},
-				{nil, nil, nil, nil},
-				{2.0, 2, "30", 4},
-				{2.0, 3, "30", 4},
-				{nil, nil, "30", nil},
-				{2.0, 3, "40", 4},
-				{2.0, 3, "40", 4},
-			},
-			expected: tuples{
-				{1.0, 2, "30", 4},
-				{nil, nil, nil, nil},
-				{2.0, 2, "30", 4},
-				{2.0, 3, "30", 4},
-				{nil, nil, "30", nil},
-				{2.0, 3, "40", 4},
-			},
-		},
-	}
-
-	for _, tc := range tcs {
-		runTests(t, []tuples{tc.tuples}, tc.expected, orderedVerifier,
-			func(input []Operator) (Operator, error) {
-				return NewOrderedDistinct(input[0], tc.distinctCols, tc.colTypes)
-			})
-	}
-}
-
-func BenchmarkSortedDistinct(b *testing.B) {
+func BenchmarkDistinct(b *testing.B) {
 	rng, _ := randutil.NewPseudoRand()
 	ctx := context.Background()
 
-	batch := coldata.NewMemBatch([]coltypes.T{coltypes.Int64, coltypes.Int64, coltypes.Int64})
-	aCol := batch.ColVec(1).Int64()
-	bCol := batch.ColVec(2).Int64()
-	lastA := int64(0)
-	lastB := int64(0)
-	for i := 0; i < int(coldata.BatchSize()); i++ {
-		// 1/4 chance of changing each distinct coldata.
-		if rng.Float64() > 0.75 {
-			lastA++
-		}
-		if rng.Float64() > 0.75 {
-			lastB++
-		}
-		aCol[i] = lastA
-		bCol[i] = lastB
+	distinctConstructors := []func(Operator, []uint32, int, []coltypes.T) (Operator, error){
+		func(input Operator, distinctCols []uint32, numOrderedCols int, typs []coltypes.T) (Operator, error) {
+			return NewOrderedDistinct(input, distinctCols, typs)
+		},
 	}
-	batch.SetLength(coldata.BatchSize())
-	source := NewRepeatableBatchSource(batch)
-	source.Init()
-
-	distinct, err := NewOrderedDistinct(source, []uint32{1, 2}, []coltypes.T{coltypes.Int64, coltypes.Int64, coltypes.Int64})
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	// don't count the artificial zeroOp'd column in the throughput
-	for _, nulls := range []bool{false, true} {
-		b.Run(fmt.Sprintf("nulls=%t", nulls), func(b *testing.B) {
-			if nulls {
-				n := coldata.NewNulls(int(coldata.BatchSize()))
-				// Setting one value to null is enough to trigger the null handling
-				// logic for the entire batch.
-				n.SetNull(0)
-				batch.ColVec(1).SetNulls(&n)
-				batch.ColVec(2).SetNulls(&n)
+	distinctNames := []string{"Ordered"}
+	orderedColsFraction := []float64{0, 0.5, 1.0}
+	for _, hasNulls := range []bool{false, true} {
+		for _, newTupleProbability := range []float64{0.001, 0.01, 0.1} {
+			for _, nBatches := range []int{1 << 2, 1 << 6} {
+				for _, nCols := range []int{2, 4} {
+					typs := make([]coltypes.T, nCols)
+					for i := range typs {
+						typs[i] = coltypes.Int64
+					}
+					batch := coldata.NewMemBatch(typs)
+					batch.SetLength(coldata.BatchSize())
+					distinctCols := []uint32{0, 1, 2, 3}[:nCols]
+					// We have the following equation:
+					//   newTupleProbability = 1 - (1 - newValueProbability) ^ nCols,
+					// so applying some manipulations we get:
+					//   newValueProbability = 1 - (1 - newTupleProbability) ^ (1 / nCols).
+					newValueProbability := 1.0 - math.Pow(1-newTupleProbability, 1.0/float64(nCols))
+					for i := range distinctCols {
+						col := batch.ColVec(i).Int64()
+						col[0] = 0
+						for j := uint16(1); j < coldata.BatchSize(); j++ {
+							col[j] = col[j-1]
+							if rng.Float64() < newValueProbability {
+								col[j]++
+							}
+						}
+						nulls := batch.ColVec(i).Nulls()
+						if hasNulls {
+							nulls.SetNull(0)
+						} else {
+							nulls.UnsetNulls()
+						}
+					}
+					for distinctIdx, distinctConstructor := range distinctConstructors {
+						numOrderedCols := int(float64(nCols) * orderedColsFraction[distinctIdx])
+						b.Run(
+							fmt.Sprintf("%s/hasNulls=%v/newTupleProbability=%.3f/rows=%d/cols=%d/ordCols=%d",
+								distinctNames[distinctIdx], hasNulls, newTupleProbability,
+								nBatches*int(coldata.BatchSize()), nCols, numOrderedCols,
+							),
+							func(b *testing.B) {
+								b.SetBytes(int64(8 * nBatches * int(coldata.BatchSize()) * nCols))
+								b.ResetTimer()
+								for n := 0; n < b.N; n++ {
+									// Note that the source will be ordered on all nCols so that the
+									// number of distinct tuples doesn't vary between different
+									// distinct operator variations.
+									source := newFiniteChunksSource(batch, nBatches, nCols)
+									distinct, err := distinctConstructor(source, distinctCols, numOrderedCols, typs)
+									if err != nil {
+										b.Fatal(err)
+									}
+									distinct.Init()
+									for b := distinct.Next(ctx); b.Length() > 0; b = distinct.Next(ctx) {
+									}
+								}
+								b.StopTimer()
+							})
+					}
+				}
 			}
-			b.SetBytes(int64(8 * coldata.BatchSize() * 3))
-			for i := 0; i < b.N; i++ {
-				distinct.Next(ctx)
-			}
-		})
+		}
 	}
 }
