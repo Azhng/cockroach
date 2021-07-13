a25d908ad82a5ff4421e10963bb7c5157c1c9ebc
Azhng <archer.xn@gmail.com>
2021-07-13 17:38:27 -0400

    wip5: some barebone deletion code

--- pkg/sql/compact_sql_stats.go
+++ pkg/sql/compact_sql_stats.go
@@ -22,6 +22,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlstats/persistedsqlstats"
+	"github.com/cockroachdb/cockroach/pkg/sql/sqlutil"
 	"github.com/cockroachdb/errors"
 )
 
@@ -41,23 +42,57 @@ func (r *resumer) Resume(ctx context.Context, execCtx interface{}) error {
 	p := execCtx.(JobExecContext)
 	ie := p.ExecCfg().InternalExecutor
 
-	err := p.ExecCfg().DB.Txn(ctx, func(ctx context.Context, txn *kv.Txn) error {
-		row, err := ie.QueryRowEx(ctx,
-			"scan-sql-stats-entries",
-			txn,
-			sessiondata.InternalExecutorOverride{User: security.NodeUserName()},
-			"SELECT count(*) FROM system.statement_statistics")
-		if err != nil {
-			return err
+	// TODO(azhng): wip: checking for running job? if such job exists the abort.
+	stmtStatsEntryCnt, txnStatsEntryCnt, err := r.getExistingStmtAndTxnStatsEntries(ctx, ie)
+	if err != nil {
+		return err
+	}
+
+	err = r.deleteOldestEntries(ctx, p.ExecCfg(), stmtStatsEntryCnt, txnStatsEntryCnt)
+	return nil
+}
+
+func (r *resumer) deleteOldestEntries(
+	ctx context.Context, execCfg *ExecutorConfig, curStmtStatsEntries, curTxnStatsEntries int64,
+) error {
+	maxStatsEntry := persistedsqlstats.SQLStatsMaxPersistedRows.Get(&execCfg.Settings.SV)
+
+	// TODO(azhng): wip: remove this.
+	fmt.Printf("Pretending to be compacting stats: stmtStatsCnt: %d, txnStatsCnt: %d, maxStatsEntry: %d\n", curStmtStatsEntries, curTxnStatsEntries, maxStatsEntry)
+
+	stmt := `
+DELETE FROM %[1]s
+WHERE fingerprint_id IN (
+  SELECT fingerprint_id
+  FROM %[1]s
+  ORDER BY aggregated_ts
+  LIMIT %[2]d
+)
+`
+	err := execCfg.DB.Txn(ctx, func(ctx context.Context, txn *kv.Txn) error {
+		if entriesToRemove := curStmtStatsEntries - maxStatsEntry; entriesToRemove > 0 {
+			_, err := execCfg.InternalExecutor.ExecEx(ctx,
+				"delete-old-stmt-stats",
+				txn,
+				sessiondata.InternalExecutorOverride{User: security.NodeUserName()},
+				fmt.Sprintf(stmt, "system.statement_statistics", entriesToRemove))
+
+			if err != nil {
+				return err
+			}
 		}
 
-		if row.Len() != 1 {
-			return errors.AssertionFailedf("unexpected number of column returned")
-		}
+		if entriesToRemove := curTxnStatsEntries - maxStatsEntry; entriesToRemove > 0 {
+			_, err := execCfg.InternalExecutor.ExecEx(ctx,
+				"delete-old-txn-stats",
+				txn,
+				sessiondata.InternalExecutorOverride{User: security.NodeUserName()},
+				fmt.Sprintf(stmt, "system.transaction_statistics", entriesToRemove))
 
-		maxStatsEntry := persistedsqlstats.SQLStatsMaxPersistedRows.Get(&p.ExecCfg().Settings.SV)
-		stmtStatsEntryCnt := (int64)(*row[0].(*tree.DInt))
-		fmt.Printf("Pretending to be compacting stats: stmtStatsCnt: %d, maxStatsEntry: %d\n", stmtStatsEntryCnt, maxStatsEntry)
+			if err != nil {
+				return err
+			}
+		}
 
 		return nil
 	})
@@ -65,7 +100,42 @@ func (r *resumer) Resume(ctx context.Context, execCtx interface{}) error {
 	if err != nil {
 		return err
 	}
+
 	return nil
+}
+
+func (r *resumer) getExistingStmtAndTxnStatsEntries(
+	ctx context.Context, ie sqlutil.InternalExecutor,
+) (stmtStatsEntryCnt, txnStatsEntryCnt int64, err error) {
+	row, err := ie.QueryRowEx(ctx,
+		"scan-sql-stmt-stats-entries",
+		nil,
+		sessiondata.InternalExecutorOverride{User: security.NodeUserName()},
+		"SELECT count(*) FROM system.statement_statistics AS OF SYSTEM TIME follower_read_timestamp()")
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if row.Len() != 1 {
+		return 0, 0, errors.AssertionFailedf("unexpected number of column returned")
+	}
+	stmtStatsEntryCnt = (int64)(*row[0].(*tree.DInt))
+
+	row, err = ie.QueryRowEx(ctx,
+		"scan-sql-txn-stats-entries",
+		nil,
+		sessiondata.InternalExecutorOverride{User: security.NodeUserName()},
+		"SELECT count(*) FROM system.transaction_statistics AS OF SYSTEM TIME follower_read_timestamp()")
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if row.Len() != 1 {
+		return 0, 0, errors.AssertionFailedf("unexpected number of column returned")
+	}
+	txnStatsEntryCnt = (int64)(*row[0].(*tree.DInt))
+
+	return stmtStatsEntryCnt, txnStatsEntryCnt, nil
 }
 
 func (r *resumer) OnFailOrCancel(ctx context.Context, execCtx interface{}) error {
